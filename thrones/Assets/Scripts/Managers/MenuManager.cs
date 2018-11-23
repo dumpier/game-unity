bc5389f0c0b0ddb88bb21d86c521bfd4cfb6bc20
noncense <noncensexnoncense@gmail.com>
2018-11-24 00:39:50 +0900

    #1 [wip]画面遷移

---
+++ thrones/Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Thrones.Managers
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public string page = "";
+
+        // ページ一覧の定義
+        public static readonly Dictionary<string, string> PageList = new Dictionary<string, string>()
+        {
+            { "home",       "HomePage" },
+            { "menu",       "MenuPage" },
+            { "quest",      "QuestPage" },
+        };
+
+
+
+        private void Start()
+        {
+            Button button = gameObject.GetComponent<Button>();
+            button.onClick.AddListener(OnClick);
+        }
+
+        public void OnClick()
+        {
+            Debug.Log($" - Click {gameObject.name} button .");
+
+            // 現在表示中のを隠す
+            PageManager.getCurrentPage().GetComponent<PageManager>().Hide();
+
+            this.ShowPage();
+        }
+
+
+        public void ShowPage()
+        {
+            this.GetPageGameObject().GetComponent<PageManager>().Show();
+        }
+
+        private GameObject GetPageGameObject()
+        {
+            Debug.Log($"page : {this.page}");
+
+            string page_name = "HomePage";
+
+            foreach (KeyValuePair<string, string> pair in MenuManager.PageList)
+            {
+                if (pair.Key == this.page)
+                {
+                    page_name = pair.Value;
+                    break;
+                }
+            }
+
+            Debug.Log($"panel name: {page_name}");
+
+            return GameObject.Find(page_name);
+        }
+    }
+}
